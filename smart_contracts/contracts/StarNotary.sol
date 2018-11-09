ebbbe65fe5d6eddc14e743210324ecd8fc7a849d
Henry Le <henryle@Henrys-Air.attlocal.net>
2018-11-08 18:01:47 -0600

    second commit

--- smart_contracts/contracts/StarNotary.sol
+++ smart_contracts/contracts/StarNotary.sol
@@ -2,22 +2,62 @@ pragma solidity ^0.4.23;
 
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
 
+
+
 contract StarNotary is ERC721 { 
 
     struct Star { 
-        string name; 
+        string name;
+        string story;
+        string ra; 
         string dec;
         string mag;
-        string cent;
     }
 
     mapping(uint256 => Star) public tokenIdToStarInfo; 
     mapping(uint256 => uint256) public starsForSale;
+    mapping(bytes32 => bool) public starCoordinateExist;
 
-    function createStar(string _name, uint256 _tokenId) public { 
-        Star memory newStar = Star(_name);
+    function concatStarCoordinate(bytes _raBytes, bytes _decBytes, bytes _magBytes) internal pure returns (bytes32) {
+        bytes memory tempValueBytes;
+        bytes32 returnValueBytes32;
+        uint offset = 0;
+        uint i;
+        uint j;
+        for(i=0; i<_raBytes.length; i++) {
+            tempValueBytes[j++] = _raBytes[i];
+        }
+        for(i=0; i<_decBytes.length; i++) {
+            tempValueBytes[j++] = _decBytes[i];
+        }
+        for(i=0; i<_magBytes.length; i++) {
+            tempValueBytes[j++] = _decBytes[i];
+        }
+
+        for (i = 0; i < 32; i++) {
+            returnValueBytes32 |= bytes32(tempValueBytes[offset + i] & 0xFF) >> (i * 8);
+        }
+
+        return returnValueBytes32;
+    }
+
+    function createStar(string _name, string _story, string _ra, string _dec, string _mag, uint256 _tokenId) public { 
+        Star memory newStar = Star(_name, _story, _ra, _dec, _mag);
+        bytes memory _raBytes = bytes(_ra);
+        bytes memory _decBytes = bytes(_dec);
+        bytes memory _magBytes = bytes(_mag);
+
+
+        //Check star coordinate must come in format: "ra_032.155", "dec_121.874", "mag_245.978"
+        require(_raBytes.length + _decBytes.length + _magBytes.length == 32,"Required coordinate format: ra_###.###, dec_###.###, mag_###.###");
+
+        //Concatenate the coordinate for look up & check Uniqless by coordinates 
+        bytes32 starCoordinate = concatStarCoordinate(_raBytes, _decBytes, _magBytes);
+
+        require(starCoordinateExist[starCoordinate] == false,"Star coorindate already exist.");
 
         tokenIdToStarInfo[_tokenId] = newStar;
+        starCoordinateExist[starCoordinate] = true;
 
         _mint(msg.sender, _tokenId);
     }
@@ -43,5 +83,38 @@ contract StarNotary is ERC721 {
         if(msg.value > starCost) { 
             msg.sender.transfer(msg.value - starCost);
         }
+    }
+
+    function checkIfStarExist(string _ra, string _dec, string _mag) public returns (bool) { 
+        bytes memory _raBytes = bytes(_ra);
+        bytes memory _decBytes = bytes(_dec);
+        bytes memory _magBytes = bytes(_mag);
+
+        //Check star coordinate must come in format: "ra_032.155", "dec_121.874", "mag_245.978"
+        require(_raBytes.length + _decBytes.length + _magBytes.length == 32,"Required coordinate format: ra_###.###, dec_###.###, mag_###.###");
+
+        bytes32 starCoordinate = concatStarCoordinate(_raBytes, _decBytes, _magBytes);
+
+        return starCoordinateExist[starCoordinate];
+    }
+
+    function mint(address to, uint256 tokenId) internal {
+        _mint(to, tokenId);
+    }
+
+    // All these function are inherited from open-zeppelin ERC721
+    // approve()
+    // safeTransferFrom()
+    // SetApprovalForAll()
+    // getApproved()
+    // isApprovedForAll()
+    // ownerOf()
+
+    function starsForSale() public {
+    }
+
+    function tokenIdToStarInfo(uint256 tokenId) public returns(string) {
+        require(_exist(tokenId), "Token does not exist");
+        return tokenIdToStarInfo[tokenId];
     }
 }
