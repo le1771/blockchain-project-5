01c5443b4e67a0ca8f87065ae4365f5905efb871
Henry Le <le1771@gmail.com>
2018-12-01 23:39:21 -0600

    Part 4 - modify client code completed

---
+++ readme.md
@@ -0,0 +1,11 @@
+
+My deployed contract on Rinkeby test net:
+Contract Address: https://rinkeby.etherscan.io/address/0x3609d300bb06d5f92faa9329f3f01c785319490d#code
+Contract TXHash: 0x8fdb5d985f3fcd7aa498b4e3d22f227832bdbd9a998b663bb9645449d0b459ff
+
+claimStar("Awesome Star!!!", "Gift for my wife!", "111.111", "222.222", "333.333", 1)
+TokenId (StarId): 1
+Transaction ID: https://rinkeby.etherscan.io/tx/0xf394b7701ab47172b57e84f9bd0e2d960135cdeda077409fa67a1cd81e817b19
+
+putStarUpForSale(1,1)
+Transaction ID: https://rinkeby.etherscan.io/tx/0x428913997217d5c0238437254553374395e88a7fa1e3eae6678b8a7fb57863c0
--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -12,7 +12,25 @@
  *   },
  */
 
+
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var mnemonic = 'like hip edit artist resist need tree proud picnic village nuclear olive';
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: { 
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: "*"
+    }, 
+    rinkeby: {
+      provider: function() { 
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/b24846f7fd524c43ac7168587086dbac') 
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    }
+  }
 };
